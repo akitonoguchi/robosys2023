6722e1e601296ac7e91d87498084928ba754fd2f
Akito <akitoniko25@gmail.com>
2023-10-26 12:38:53 +0900

    Add a test case

--- yamada.bash
+++ yamada.bash
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-a=山田
-[ "$a" = 上田 ]
-echo $?
-[ "$a" = 山田 ]
-echo $?
+ng () {
+	echo ${1}行目が違うよ #$1はngの１番目の引数
+	ret=1			#最後に返す終了ステータスをセット
+}
 
+ret=0
+a=山田
+[ "$a" = 上田 ] || ng "$LINENO"
+[ "$a" = 山田 ] || ng "$LINENO"
+
+exit $[ret]
