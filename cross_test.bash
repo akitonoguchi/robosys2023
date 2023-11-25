b886baf09ec27f4c9d61af722d6ecd075a3c3ff3
Akito <akitoniko25@gmail.com>
2023-11-25 17:03:14 +0900

    Add a test

---
+++ cross_test.bash
@@ -0,0 +1,25 @@
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2023 Akito Noguchi
+# SPDX-License-Identifier: BSD-3-Clause
+
+ng () {
+      echo NG at Line $1
+      res=1
+}
+res=0
+
+### I/O TEST ###
+out=$(seq 5 | ./cross)
+[ "${out}" = 120 ] || ng ${LINENO}
+
+### STRANGE INPUT ###
+out=$(echo あ | ./cross)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}    　
+
+out=$(echo | ./cross)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}　
+
+[ "$res" = 0 ] && echo OK
+exit $res
