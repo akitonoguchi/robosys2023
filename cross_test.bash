1517c2e8509dedf281c840494505208f2e93a0fc
Akito <akitoniko25@gmail.com>
2023-11-25 16:32:45 +0900

    Check cross_test

--- cross_test.bash
+++ cross_test.bash
@@ -1,5 +1,17 @@
-#!/bin/bash
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2023 Akito Noguchi
+# SPDX-License-Identifier: BSD-3-Clause
 
+ng () {
+      echo NG at Line $1
+      res=1
+}
+　
+res=0
+
+### I/O TEST ###
 out=$(seq 5 | ./cross)
+[ "${out}" = 120 ] || ng ${LINENO}
 
-[ "${out}" = 121 ]
+[ "$res" = 0 ] && echo OK
+exit $res
