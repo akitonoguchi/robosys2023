87f93c90194b0993d1a82dea63c808071ca6ef47
Akito <akitoniko25@gmail.com>
2023-11-26 20:23:39 +0900

    Add a test

--- cross_test.bash
+++ cross_test.bash
@@ -12,6 +12,27 @@ res=0
 out=$(seq 5 | ./cross)
 [ "${out}" = 120 ] || ng ${LINENO}
 
+out=$(seq 5 | sed 's/$/.1/' | ./cross)
+[ "${out}" = 149.73651 ] || ng ${LINENO}
+
+out=$(seq 10 | ./cross)
+[ "${out}" = 3628800 ] || ng ${LINENO}
+
+out=$(seq 3  10 | ./cross)
+[ "${out}" = 1814400 ] || ng ${LINENO}
+
+out=$(seq 1 2 15 | ./cross)
+[ "${out}" = 2027025 ] || ng ${LINENO}
+
+out=$(seq 10 -3 -2 | ./cross)
+[ "${out}" = -560 ] || ng ${LINENO}
+
+out=$(echo 1 2 3 4 5 6 | tr ' ' '\n' | ./cross)
+[ "${out}" = 720 ] || ng ${LINENO}
+
+out=$(echo 1 3 5 7 9 11 | tr ' ' '\n' | ./cross)
+[ "${out}" = 10395 ] || ng ${LINENO}
+
 ### STRANGE INPUT ###
 out=$(echo あ | ./cross)
 [ "$?" = 1 ]      || ng ${LINENO}
@@ -20,6 +41,18 @@ out=$(echo あ | ./cross)
 out=$(echo | ./cross)
 [ "$?" = 1 ]      || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}　
+
+out=$(echo akito | ./cross)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo seq 5 | ./cross)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo @ | ./cross)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 exit $res
